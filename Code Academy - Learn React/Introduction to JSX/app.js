c668adca59d7217ea4b798d2d92d9d0bcb0326a1
SamuelJayDavies <samj.davies@virginmedia.com>
2022-09-12 16:23:11 +0100

    This might work later, react hasn't bene set up

---
+++ Code Academy - Learn React/Introduction to JSX/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = 
+(
+  <ul>
+    <li>Argument 1</li>
+    <li>Argument 2</li>
+    <li>Final argument</li>
+  </ul>
+);
+
+ReactDOM.render(myList, document.getElementById('app'));
