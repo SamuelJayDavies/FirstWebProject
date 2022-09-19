0c60812d3cd736966a6d7c729015adf55a15374d
SamuelJayDavies <samj.davies@virginmedia.com>
2022-09-19 20:05:21 +0100

    Replaced the system using props

--- Web-Development-Projects/ravenous/src/components/SearchBar/SearchBar.js
+++ Web-Development-Projects/ravenous/src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import './SearchBar.css';
 
 const sortByOptions = {
-    'Best Match:': 'best_match',
+    'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
 }
