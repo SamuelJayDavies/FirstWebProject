ec04cb91587dea9b8193cfae068b449a68779426
SamuelJayDavies <samj.davies@virginmedia.com>
2022-09-21 16:35:42 +0100

    Kinda works I guess

---
+++ Web-Development-Projects/ravenous-v2/src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'ibBGf9CHwk1ukcpJWqqXdxLown-j5RkNREs6qCxYR_xKHrYyN8qV6qa1y_lPR-RWiHv0wY05bL_6ZtgpTcg8l7yjvXUKtxQQyqwE1rUZQgWxUA26E6zV3FBCKywrY3Yx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
